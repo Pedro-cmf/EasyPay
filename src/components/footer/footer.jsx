1dd3e158927dbdf33735d4e77df2ea430d885ea3
Pedro César <pedroocesarmf@gmail.com>
2025-04-19 22:31:39 -0300

    Trocando numero footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import { FooterContainer } from './footerStyles';
 function Footer() {
   return (
     <FooterContainer>
-      © 2024 EasyPay Instituição de Pagamento S/A. CNPJ 00.000.000/0000-00 Edifício Los Santos Brasília - DF
+      © 2024 EasyPay Instituição de Pagamento S/A. CNPJ 00.000.000/0000-21 Edifício Los Santos Brasília - DF
     </FooterContainer>
   );
 }
